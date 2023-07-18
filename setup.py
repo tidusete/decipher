d8588224fc1dd02e2663f7f46f6a0155f35d26b0
tidusete <jordicidsierra@gmail.com>
2023-07-18 22:50:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url='https://https://github.com/tidusete/decipher',
     author='dsymbol',
     install_requires=[
-        'openai-whisper @ git+https://github.com/openai/whisper.git',
+        'openai-whisper @ git+https://github.com/openai/whisper.git@b91c907694f96a3fb9da03d4bbdc83fbcd3a40a4',
         'click==8.1.3',
         'ffpb==0.4.1',
         'tqdm'
